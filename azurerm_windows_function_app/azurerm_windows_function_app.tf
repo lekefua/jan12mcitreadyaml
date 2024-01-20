42418fa50db697160e0f0cb9377d084c14135ba4
lekefua <145169940+lekefua@users.noreply.github.com>
2024-01-20 01:49:27 +0100

    Update azurerm_windows_function_app.tf

--- azurerm_windows_function_app/azurerm_windows_function_app.tf
+++ azurerm_windows_function_app/azurerm_windows_function_app.tf
@@ -1,1 +1,58 @@
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region where resources will be created"
+  type        = string
+}
+
+variable "vnet_name" {
+  description = "Name of the Virtual Network"
+  type        = string
+}
+
+variable "subnet_web_name" {
+  description = "Name of the Web Subnet"
+  type        = string
+}
+
+variable "subnet_app_name" {
+  description = "Name of the Application Subnet"
+  type        = string
+}
+
+variable "subnet_db_name" {
+  description = "Name of the Database Subnet"
+  type        = string
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = var.vnet_name
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_subnet" "web" {
+  name                 = var.subnet_web_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "app" {
+  name                 = var.subnet_app_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_subnet" "db" {
+  name                 = var.subnet_db_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
 
