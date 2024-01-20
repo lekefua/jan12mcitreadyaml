c57de45ef8dffadb51494a2f6f580c44441266b0
lekefua <145169940+lekefua@users.noreply.github.com>
2024-01-20 01:50:22 +0100

    Update variables.tf

--- variables/variables.tf
+++ variables/variables.tf
@@ -1,1 +1,30 @@
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region where resources will be created"
+  type        = string
+}
+
+variable "vnet_name" {
+  description = "Name of the Virtual Network"
+  type        = string
+}
+
+variable "subnet_web_name" {
+  description = "Name of the Web Subnet"
+  type        = string
+}
+
+variable "subnet_app_name" {
+  description = "Name of the Application Subnet"
+  type        = string
+}
+
+variable "subnet_db_name" {
+  description = "Name of the Database Subnet"
+  type        = string
+}
 
